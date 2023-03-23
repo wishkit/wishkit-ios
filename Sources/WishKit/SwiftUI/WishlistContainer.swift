0d3ee1c1a3f8f1414c808967ef3c5d66efdf30c3
Martin Lasek <heylasek@gmail.com>
2023-03-23 13:51:59 -0700

    macOS now supports hiding the segmented control configuration

--- Sources/WishKit/SwiftUI/WishlistContainer.swift
+++ Sources/WishKit/SwiftUI/WishlistContainer.swift
@@ -12,6 +12,9 @@ import WishKitShared
 #if os(macOS)
 struct WishlistContainer: View {
 
+    @Environment(\.colorScheme)
+    var colorScheme
+
     @State
     private var listType: WishState = .approved
 
@@ -32,34 +35,60 @@ struct WishlistContainer: View {
 
     var body: some View {
         VStack {
-            ZStack {
-                Picker(selection: $listType, content: {
-                    Text("Approved").tag(WishState.approved)
-                    Text("Implemented").tag(WishState.implemented)
-                }, label: {
-                    EmptyView()
-                })
-                .pickerStyle(SegmentedPickerStyle())
-                .padding()
-                .frame(maxWidth: 300)
 
-                HStack {
-                    Button(action: refreshList) {
-                        if isRefreshing {
-                            ProgressView()
-                                .scaleEffect(0.4)
-                                .progressViewStyle(CircularProgressViewStyle())
-                        } else {
-                            Image(systemName: "arrow.clockwise")
+            if WishKit.configuration.showSegmentedControl {
+                ZStack {
+                    Picker(selection: $listType, content: {
+                        Text("Approved").tag(WishState.approved)
+                        Text("Implemented").tag(WishState.implemented)
+                    }, label: {
+                        EmptyView()
+                    })
+                    .pickerStyle(SegmentedPickerStyle())
+                    .padding()
+                    .frame(maxWidth: 300)
+
+                    HStack {
+                        Button(action: refreshList) {
+                            if isRefreshing {
+                                ProgressView()
+                                    .scaleEffect(0.4)
+                                    .progressViewStyle(CircularProgressViewStyle())
+                            } else {
+                                Image(systemName: "arrow.clockwise")
+                            }
                         }
+                        .buttonStyle(PlainButtonStyle())
+                        .frame(width: 20, height: 20)
+                        .padding(EdgeInsets(top: 0, leading: 315, bottom: 0, trailing: 0))
                     }
-                    .buttonStyle(PlainButtonStyle())
-                    .frame(width: 20, height: 20)
-                    .padding(EdgeInsets(top: 0, leading: 315, bottom: 0, trailing: 0))
                 }
+            } else {
+                Button(action: refreshList) {
+                    if isRefreshing {
+                        ProgressView()
+                            .scaleEffect(0.4)
+                            .progressViewStyle(CircularProgressViewStyle())
+                    } else {
+                        Image(systemName: "arrow.clockwise")
+                    }
+                }
+                .buttonStyle(PlainButtonStyle())
+                .frame(width: 20, height: 20)
+                .padding(EdgeInsets(top: 15, leading: 0, bottom: 15, trailing: 0))
             }
 
             WishlistView(wishModel: wishModel, listType: $listType)
+            
+        }.background(systemBackgroundColor)
+    }
+
+    var systemBackgroundColor: Color {
+        switch colorScheme {
+        case .light:
+            return PrivateTheme.systemBackgroundColor.light
+        case .dark:
+            return PrivateTheme.systemBackgroundColor.dark
         }
     }
 }
