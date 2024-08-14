cd2bdc7d98acef37ff61b11de15fabe84c8b8778
Martin Lasek <m_lasek@apple.com>
2024-08-14 13:26:51 -0700

    maintenance

--- Sources/WishKit/SwiftUI/WishView.swift
+++ Sources/WishKit/SwiftUI/WishView.swift
@@ -47,7 +47,7 @@ struct WishView: View {
         self.wishResponse = wishResponse
         self.viewKind = viewKind
         self.voteActionCompletion = voteActionCompletion
-        self._voteCount = State(wrappedValue: wishResponse.votingUsers.count)
+        self._voteCount.wrappedValue = wishResponse.votingUsers.count
     }
 
     var body: some View {
